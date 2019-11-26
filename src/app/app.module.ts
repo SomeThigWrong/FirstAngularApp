4ba1b246c2734ac1d4679dc239798ee4a1cddf32
Cường Bảo <cuongdajca0944@outlook.com>
2019-11-27 00:55:35 +0700

    Add a testing component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyfirstcomponentComponent } from './myfirstcomponent/myfirstcomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyfirstcomponentComponent
   ],
   imports: [
     BrowserModule,
