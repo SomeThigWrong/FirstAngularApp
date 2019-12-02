59499448437d09fdbfc131e92f49cdf503f08045
cuonglb <cuonglb@ominext.com>
2019-12-02 17:05:31 +0700

    Add first component

--- src/app/myfirstcomponent/myfirstcomponent.component.ts
+++ src/app/myfirstcomponent/myfirstcomponent.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./myfirstcomponent.component.css']
 })
 export class MyfirstcomponentComponent implements OnInit {
-
+  public greeting = "";
   constructor() { }
 
   ngOnInit() {
   }
-
+  sayHello(you){
+    console.log(you);
+    this.greeting = "cuonglb";
+  }
+  logMessage(value){
+    console.log(value)
+  }
 }
