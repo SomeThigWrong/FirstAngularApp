f45e86f8c39e3097dfd5c14fdea10c5ecfc358a0
cuonglb <cuonglb@ominext.com>
2020-01-08 16:03:21 +0700

    Add page not found and default routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DepartmentListComponent } from './department-list/department-list.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
+  {path:"",redirectTo:"/department",pathMatch:'full'},
   {path:"department",component:DepartmentListComponent},
   {path:"employee",component:EmployeeListComponent},
+  //should be in the final route declaration
+  {path:"**",component:PageNotFoundComponent},
 ];
 
 @NgModule({
@@ -14,4 +18,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const  routingComponents = [DepartmentListComponent,EmployeeListComponent]
+export const  routingComponents = [DepartmentListComponent,EmployeeListComponent,PageNotFoundComponent]
