e63befbf9b4a9d678a6477e013b0570305667815
cuonglb <cuonglb@ominext.com>
2020-01-08 15:43:32 +0700

    add Navigate and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyfirstcomponentComponent } from './myfirstcomponent/myfirstcomponent.component';
 import { EmployeeService } from './employee.service';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyfirstcomponentComponent
+    MyfirstcomponentComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
